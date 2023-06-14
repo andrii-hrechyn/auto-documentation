14025578f11db8a6f3d57e6b422d69a487942f83
Andriy Hrechyn <andriy.hrechyn@gmail.com>
2023-06-14 22:46:12 +0200

    fixed array property

--- src/Properties/ArrayProperty.php
+++ src/Properties/ArrayProperty.php
@@ -11,7 +11,7 @@ class ArrayProperty extends Property
 {
     public static function make(string $name, SchemaComponent|Schema $items): static
     {
-        if ($items instanceof Schema) {
+        if ($items instanceof Schema || $items instanceof SchemaComponent) {
             $items = ArraySchema::make($items);
         }
 
